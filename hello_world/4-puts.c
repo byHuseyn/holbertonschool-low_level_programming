841f715e2bd863ae74ef0531c7d6b62f56aff1af
byHuseyn <ismayilovhuseyn.info@gmail.com>
2025-07-25 20:37:45 +0000

    task

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message using puts
+ * return: Always 0 (Success)
+ */
+
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 
-return 0;
+return (0);
 }
