81656b0378a61e5e0348bfb1fc4855e662a74b57
byHuseyn <ismayilovhuseyn.info>
2025-07-26 09:11:49 +0000

    Task 6: Size of variables

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Sizeof function that prints the size of various types
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
+
