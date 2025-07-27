8832c9d0595ce9bfa8da411b325ccb1476e50ac2
byHuseyn <ismayilovhuseyn.info@gmail.com>
2025-07-27 15:37:30 +0000

    Tasks

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * main - Entry point
  * print_last_digit - prints the last digit of a number
+ * @n: The number from which the last digit will be extracted
  *
- * Return: last digit
+ * Return: The value of the last digit
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last;
 
-	if (last_digit < 0)
-		last_digit = -last_digit;
+	last = n % 10;
+	if (last < 0)
+		last *= -1;
 
-	_putchar(last_digit + '0');
-	return (last_digit);
+	_putchar(last + '0');
+	return (last);
 }
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table 
+ */
+void times_table(void)
+{
+	int i, h, timetable;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (h = 0; h <= 9; h++)
+		{
+			timetable = i * h;
+
+			if (h == 0)
+				_putchar(timetable + '0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (timetable < 10)
+				{
+					_putchar(' ');
+					_putchar(timetable + '0');
+				}
+				else
+				{
+					_putchar((timetable / 10) + '0');
+					_putchar((timetable % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
