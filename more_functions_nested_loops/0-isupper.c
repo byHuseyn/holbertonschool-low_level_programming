1ddb763a5d738b571c1f05801f2eac8a8ca6d1db
byHuseyn <ismayilovhuseyn.info@gmail.com>
2025-07-31 16:26:29 +0000

    Tasks

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @c - Checker
+ *
+ * Return: If c is upppercase 1, otherwise 0
+ */
+int _isuppper(int c);
+{
+:
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Created for C programming language
+
