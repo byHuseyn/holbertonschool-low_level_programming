f263814ce215948a8abd1a15ca67582fa3c42b28
byHuseyn <ismayilovhuseyn.info@gmail.com>
2025-08-02 13:04:44 +0000

    Tasks

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - Prints the n number of times the character _
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+    int i;
+
+    if (n <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        for (i = 0; i < n; i++)
+        {
+            _putchar('_');
+        }
+        _putchar('\n');
+    }
+}
