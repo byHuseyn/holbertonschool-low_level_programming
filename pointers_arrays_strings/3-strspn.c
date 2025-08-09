e7a1e5eb92bd55395d8b540ab68726ccfb2678bc
byHuseyn <isamyilovhuseyn.info@gmail.com>
2025-08-09 14:06:04 +0000

    TASK

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strspn - Return number bytes  in the initial segmen
+ * @accept: Bytes from initial segment
+ * @s: Checked string
+ *
+ * Return: Numbers of Symbol
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i, found;
+
+	while (*s)
+	{
+		found = 0;
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				found = 1;
+				break;
+			}
+		}
+
+		if (!found)
+			break;
+		count++;
+		s++;
+	}
+	return count;
+}
