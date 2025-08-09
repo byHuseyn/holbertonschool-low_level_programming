e38ae84a149da5ca8182cac47f472f469d4ccd7f
byHuseyn <ismayilovhuseyn.info@gmail.com>
2025-08-09 22:35:08 +0000

    TASKS

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - Function locates the first occurrence in the string
+ * @s: String
+ * @accept: string with characters to match
+ *
+ * Return: pointer to first match, or NULL if no match
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,5 @@
+#include "main.h"
+/**
+ * _strstr - Function finds the first occurrence of the substring
+ * @needle: 
+ * @haystack: 
