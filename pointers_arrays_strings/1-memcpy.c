337841fb9e39589abe67d907db93a2185322382a
byHuseyn <ismayilovhuseyn.info@gmail.com>
2025-08-09 11:17:03 +0000

    TASKS

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - Copies function
+ * @src: Memory area
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int z;
+
+	for (z = 0; z < n; z++)
+	{
+		dest[z] = src[z];
+	}
+
+	return (dest);
+}
---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strchr - Function first occurrence of the character
+ * @s: String
+ *
+ * Return: c
+ */
+char *_strchr(char *s, char c)
+{
+	char c;
+	while (c =
